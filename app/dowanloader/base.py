3cd302e6de7564f8b7758657df2a803731a2a617
administrator <jean.pan9@qq.com>
2020-01-04 19:09:58 +0800

    Promote progress

--- app/dowanloader/base.py
+++ app/dowanloader/base.py
@@ -1,7 +1,22 @@
 import logging
 
+
 class BaseDownloader:
-    def __init__(self,extractor):
-        self.url = url  # 下载地址
-        self.path = path  # 下载路径
-        self.chunk_size = 16384
+    def __init__(self, extractor):
+        self.session = extractor.session
+
+        self._chunk_size = 16384
+        self._total_size = None
+
+    def download(self):
+        self.pre_progress()
+
+
+
+        self.post_progress()
+
+    def pre_progress(self):
+        pass
+
+    def post_progress(self):
+        pass
