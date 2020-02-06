e0038c1862d6424c81f4a5d2a36da71afd92695d
administrator <jean.pan9@qq.com>
2020-02-06 18:02:20 +0800

    Promote progress

---
+++ app/extractor/wallpapercraft.py
@@ -0,0 +1,65 @@
+import os
+import bs4
+import lxml
+import mimetypes
+from .common import Extractor
+
+
+class WallpaperCraftExtractor(Extractor, catalog=None, sort=None, resolution=None):
+    filename_fmt = '{id}'
+
+    def __init__(self, catalog, sort, resolution):
+        super().__init__()
+
+        self.category = 'wallpapercraft'
+        self.root = self.config('Root')
+
+        if catalog and sort and resolution:
+            catalog = 'catalog/' + catalog
+            self.url = '{}/{}/{}/{}'.format(self.root, catalog, sort, resolution)
+        elif catalog:
+            pass
+
+        elif sort:
+            self.url = '{}/{}/{}'.format(self.root, catalog, sort)
+        elif resolution:
+            self.url = '{}/{}/{}'.format(self.root, catalog, resolution)
+
+        self.is_last_page = False
+
+    def next(self):
+        self.links.clear()
+        if self.is_last_page:
+            return False
+        else:
+            self._get_page_link()
+            return True
+
+    def filename(self, response):
+        basename = os.path.basename(response.request.url)
+        url = basename.split('%20')
+        extension = mimetypes.guess_extension(response.headers.get('Content-Type'))
+        return self.filename_fmt.format(id=url[2], extension=extension)
+
+    def _get_page_link(self):
+        print(self.url)
+        response = self.session.get(url=self.url)
+        bs = bs4.BeautifulSoup(response.text, 'lxml')
+        self._find_page_link(bs)
+        next_page = self._find_next_page(bs)
+        if next_page is None:
+            self.is_last_page = True
+        else:
+            self.url = next_page
+
+    def _find_page_link(self, bs):
+        links = bs.find_all('a', class_='directlink')
+        for link in links:
+            self.links.append(link.get('href'))
+
+    def _find_next_page(self, bs):
+        next_page = bs.find('a', class_='next_page')
+        if next_page:
+            return self.root + next_page.get('href')
+        else:
+            return None
