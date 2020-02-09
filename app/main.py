eca8b135f22a3ad2ba288d3c6b24ac0ca508f8d3
manjaro <jonpan57@qq.com>
2020-02-09 16:55:26 +0800

    fix

--- app/main.py
+++ app/main.py
@@ -4,14 +4,13 @@ import bs4
 import os
 
 # from app.extractor.bing import BingExtractor
-# from .extractor.konachan import KonachanExtractor
+from .extractor.konachan import KonachanExtractor
 from .extractor.wallpapercraft import WallpaperCraftExtractor
 from .dowanloader.http import HttpDownloader
 from .processor.common import Processor
 
-# konachan = KonachanExtractor()
-# downloader = HttpDownloader(konachan)
-# processor = Processor()
-# processor.submit(downloader.download, konachan)
+konachan = KonachanExtractor()
+downloader = HttpDownloader(konachan)
+processor = Processor()
+processor.submit(downloader.download, konachan)
 
-wallpapercraft=WallpaperCraftExtractor(sort='date',resolution='1024#765')
