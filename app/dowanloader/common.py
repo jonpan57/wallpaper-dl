fd52b2b79f8ce0fd1e16c30a89f367bd807685e4
Jon Pan <jonpan57@gmail.com>
2020-06-05 17:39:10 +0800

    Update common.py

--- app/dowanloader/common.py
+++ app/dowanloader/common.py
@@ -1,38 +1,20 @@
 import os
-import logging
+from app.log import Log
 from app.config import Config
 
 
 class Downloader:
     category = 'downloader'
-    config = Config(category)
+    subcategory = ''
 
     def __init__(self, extractor):
         self.session = extractor.session
 
-        self._retries = int(self.config('Retries'))
-        self._timeout = int(self.config('Timeout'))
-        self._stream = bool(self.config('Stream'))
-        self._verify = bool(self.config('Verify'))
-        self._chunk_size = int(self.config('ChunkSize'))
+        self.config = Config(self.category)
+        self.log = Log('{}.{}'.format(self.category, self.subcategory))
 
-    def download(self, url, path_fmt):
-        # 以后加入覆盖下载和中止下载选项
-        try:
-            self._start_download(url, path_fmt)
-        except Exception as e:
-            raise e
-        finally:
-            self._end_download(url, path_fmt)
+        self.part = self.config('part')
+        self.partdir = self.config('partdir')
 
-    def _start_download(self, url, **options):
-        pass
-
-    def _end_download(self, url):
-        pass
-
-    def pre_progress(self):
-        pass
-
-    def post_progress(self):
-        pass
+    def download(self, url, pathfmt):
+        '''从url下载到path_fmt'''
