245eee7b40ee045b1470e24a99fe7dfd298a1801
manjaro <jonpan57@gmail.com>
2020-11-25 17:31:16 +0800

    init

--- app/dowanloader/common.py
+++ app/dowanloader/common.py
@@ -9,16 +9,15 @@ class Downloader:
 
     def __init__(self, job):
         self.out = job.out
-        self.session = job.extractor.session
+        self.session = job.session
         self.config = config.Config(self.category)
 
         self.default = self.config['default', 'bool']
-        if self.default:
-            self.default_directory = self.config['default-directory']
-        else:
-            self.default_directory=
+        self.file_directory = self.config['default-directory'] if self.default else self.config['custom-directory']
         # 默认路径情况下，可以添加功能，根据文件类型自动识别到图片、音乐、视频、文档等文件夹；根据URL自动识别到对应的文件夹
+
         self.temp = self.config['temp', 'bool']  # 是否开启临时缓存文件下载
+        self.temp_directory = self.config['temp-directory'] if self.temp else '/'
 
         self.log = log.Log(self.category + '.' + self.subcategory)
         # 添加临时文件夹判断
